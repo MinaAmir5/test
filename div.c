9d24730012e0324d9de9748bcbc117deb9679210
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-21 02:41:43 +0300

    update

--- div.c
+++ div.c
@@ -1,40 +1,36 @@
 #include "monty.h"
 /**
  * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * @Copy_Stack_Head: stack head
+ * @Copy_U32_Counter: line number
 */
-void f_div(stack_t **head, unsigned int counter)
+void f_div(stack_t **Copy_Stack_Head, unsigned int Copy_U32_Counter)
 {
-	stack_t *h;
-	int len = 0, aux;
+	stack_t *Local_Stack_Queue;
 
-	h = *head;
-	while (h)
+	Local_Stack_Queue = *Copy_Stack_Head;
+	if (Local_Stack_Queue && Local_Stack_Queue->next)
 	{
-		h = h->next;
-		len++;
+		if (Local_Stack_Queue->n == 0)
+		{
+			fprintf(stderr, "L%d: division by zero\n", Copy_U32_Counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*Copy_Stack_Head);
+			exit(EXIT_FAILURE);
+		}
+		Local_Stack_Queue->next->n = Local_Stack_Queue->next->n /
+			Local_Stack_Queue->n;
+		*Copy_Stack_Head = Local_Stack_Queue->next;
+		free(Local_Stack_Queue);
 	}
-	if (len < 2)
+	else
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't div, stack too short\n", Copy_U32_Counter);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		free_stack(*Copy_Stack_Head);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+
 }
