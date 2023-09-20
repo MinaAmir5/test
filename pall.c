b314c512a99ef3115babd78688be905d63539c19
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-21 00:30:34 +0300

    update

--- pall.c
+++ pall.c
@@ -1,19 +1,18 @@
 #include "monty.h"
 /**
  * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
+ * @Copy_Stack_Head: stack head
+ * @Copy_U32_Counter: not used
 */
-void f_pall(stack_t **head, unsigned int counter)
+void f_pall(stack_t **Copy_Stack_Head, unsigned int Copy_U32_Counter)
 {
-	stack_t *h;
-	(void)counter;
+	stack_t *Local_Stack_Queue;
+	(void)Copy_U32_Counter;
 
-	h = *head;
-	while (h)
+	Local_Stack_Queue = *Copy_Stack_Head;
+	while (Local_Stack_Queue)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", Local_Stack_Queue->n);
+		Local_Stack_Queue = Local_Stack_Queue->next;
 	}
 }
