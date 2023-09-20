d1eef7f885ae1f11cc25719df98f19bade371a8c
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-20 23:36:32 +0300

    update

--- push.c
+++ push.c
@@ -7,7 +7,7 @@
 */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+	int n, j = 0;
 
 	if (bus.arg)
 	{
@@ -16,13 +16,15 @@ void f_push(stack_t **head, unsigned int counter)
 		for (; bus.arg[j] != '\0'; j++)
 		{
 			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", counter);
+				fclose(bus.file);
+				free(bus.content);
+				free_stack(*head);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 	else
 	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
