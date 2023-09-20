8c1f05e4dd22867d3ce21e4672d2372edff0b9f7
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-20 21:57:48 +0300

    update

--- main.c
+++ main.c
@@ -8,39 +8,39 @@ bus_t bus = {NULL, NULL, NULL, 0};
 */
 int main(int Copy_U32_ArgNo, char *Copy_U32_Env[])
 {
+	unsigned int Local_U32_Counter = 0;
+	FILE *Local_File;
+	stack_t *stack = NULL;
 	char *content;
-	FILE *file;
 	size_t size = 0;
 	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
 
 	if (Copy_U32_ArgNo != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(Copy_U32_Env[1], "r");
-	bus.file = file;
-	if (!file)
+	Local_File = fopen(Copy_U32_Env[1], "r");
+	bus.file = Local_File;
+	if (!Local_File)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", Copy_U32_Env[1]);
 		exit(EXIT_FAILURE);
 	}
 	content = NULL;
-	read_line = getline(&content, &size, file);
+	read_line = getline(&content, &size, Local_File);
 	bus.content = content;
-	counter++;
-	for (counter = 1; read_line > 0; counter++)
+	Local_U32_Counter++;
+	for (Local_U32_Counter = 1; read_line > 0; Local_U32_Counter++)
 	{
-		execute(content, &stack, counter, file);
+		execute(content, &stack, Local_U32_Counter, Local_File);
 		free(content);
 		content = NULL;
-		read_line = getline(&content, &size, file);
+		read_line = getline(&content, &size, Local_File);
 		bus.content = content;
 	}
 	free(content);
 	free_stack(stack);
-	fclose(file);
+	fclose(Local_File);
 return (0);
 }
