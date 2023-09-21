e9ca0b30ffb6ffe5205cee85e2a227b0104e2101
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-21 04:54:04 +0300

    update

--- pstr.c
+++ pstr.c
@@ -1,19 +1,20 @@
 #include "monty.h"
 /**
- * f_pstr - prints the string starting at the top of the stack,
- * @head: stack head
- * @counter: line_number
+ * f_pstr - prints the string starting from the top of the stack
+ * @Copy_Stack_Head: stack head
+ * @Copy_U32_Counter: not used
 */
-void f_pstr(stack_t **head, unsigned int counter)
+void f_pstr(stack_t **Copy_Stack_Head, unsigned int Copy_U32_Counter)
 {
-	stack_t *h;
-	(void)counter;
+	stack_t *Local_Stack_Queue;
+	(void)Copy_U32_Counter;
 
-	h = *head;
-	while (h && (h->n <= 127 && h->n > 0))
+	Local_Stack_Queue = *Copy_Stack_Head;
+	while (Local_Stack_Queue && (Local_Stack_Queue->n <= 127 &&
+		Local_Stack_Queue>n > 0))
 	{
-		printf("%c", h->n);
-		h = h->next;
+		printf("%c", Local_Stack_Queue->n);
+		Local_Stack_Queue = Local_Stack_Queue->next;
 	}
 	printf("\n");
 }
