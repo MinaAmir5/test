65bafdda3abb17454f2039b5bae7a1f8591b0350
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-21 05:12:12 +0300

    update

--- rotr.c
+++ rotr.c
@@ -4,12 +4,13 @@
   *@Copy_Stack_Head: stack head
   *@counter: line number
  */
-void f_rotr(stack_t **Copy_Stack_Head, __attribute__((unused)) unsigned int counter)
+void f_rotr(stack_t **Copy_Stack_Head, __attribute__((unused)) unsigned int
+	Copy_U32_Counter)
 {
 	stack_t *Local_Stack_Copy;
 
 	Local_Stack_Copy = *Copy_Stack_Head;
-	if (Local_Stack_Queue && Local_Stack_Queue->next)
+	if (Local_Stack_Copy && Local_Stack_Copy->next)
 	{
 		while (Local_Stack_Copy->next)
 			Local_Stack_Copy = Local_Stack_Copy->next;
