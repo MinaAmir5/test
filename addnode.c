4593880c439b88f5edb3b060dceeb8e9aaaee512
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-22 02:09:30 +0300

    Update addnode.c

--- addnode.c
+++ addnode.c
@@ -4,7 +4,7 @@
  * @Copy_Stack_Head: stack  head
  * @Copy_U32_Value: new value
 */
-void addnode(stack_t **Copy_Stack_Head, int Copy_U32_Value)
+void Void_AddStack(stack_t **Copy_Stack_Head, int Copy_U32_Value)
 {
 
 	stack_t *Local_Stack_NewNode, *Local_Stack_Head;
