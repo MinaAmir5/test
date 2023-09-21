ce83f8ff573000147eda7a9413471f7841d6bd5b
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-22 01:14:40 +0300

    update

--- stack.c
+++ stack.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 /**
- * f_stack - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * f_stack - select LIFO
+ * @Copy_Stack_Head: stack head
+ * @Copy_U32_Counter: line number
 */
-void f_stack(stack_t **head, unsigned int counter)
+void f_stack(stack_t **Copy_Stack_Head, unsigned int Copy_U32_Counter)
 {
-	(void)head;
-	(void)counter;
+	(void)Copy_Stack_Head;
+	(void)Copy_U32_Counter;
 	bus.lifi = 0;
 }
