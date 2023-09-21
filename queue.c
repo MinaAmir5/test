c65b03b3e8da44d2b227afdd9e9f2c42c2281636
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-21 20:33:48 +0300

    update

--- queue.c
+++ queue.c
@@ -20,19 +20,16 @@ void addqueue(stack_t **head, int n)
 {
 	stack_t *new_node, *aux;
 
-	aux = *head;
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
 		printf("Error\n");
 	}
+	aux = *head;
 	new_node->n = n;
 	new_node->next = NULL;
-	if (aux)
-	{
-		while (aux->next)
-			aux = aux->next;
-	}
+	while ((*head) && aux->next)
+		aux = aux->next;
 	if (!aux)
 	{
 		*head = new_node;
