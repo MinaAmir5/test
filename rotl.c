813bbab7dc03a9ef975b13b15744a43c3935c9a7
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-21 20:08:04 +0300

    update

--- rotl.c
+++ rotl.c
@@ -9,18 +9,17 @@ void f_rotl(stack_t **head, unsigned int counter)
 	stack_t *tmp = *head, *aux;
 	(void)counter;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (tmp && tmp->next)
 	{
-		return;
+		aux = (*head)->next;
+		aux->prev = NULL;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = *head;
+		(*head)->next = NULL;
+		(*head)->prev = tmp;
+		(*head) = aux;
 	}
-	aux = (*head)->next;
-	aux->prev = NULL;
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-	}
-	tmp->next = *head;
-	(*head)->next = NULL;
-	(*head)->prev = tmp;
-	(*head) = aux;
 }
