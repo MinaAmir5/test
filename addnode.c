51601248dc9f1240af28a7f3196d7c0b0790a41f
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-21 19:01:18 +0300

    update

--- addnode.c
+++ addnode.c
@@ -3,22 +3,23 @@
  * addnode - add node to the head stack
  * @head: head of the stack
  * @n: new_value
- * Return: no return
 */
 void addnode(stack_t **head, int n)
 {
 
 	stack_t *new_node, *aux;
 
-	aux = *head;
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
-	{ printf("Error\n");
-		exit(0); }
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	aux = *head;
 	if (aux)
 		aux->prev = new_node;
+	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = *head;
-	new_node->prev = NULL;
 	*head = new_node;
 }
