a0ad7c503a3e649dff3b5b324ffdd0505368e93d
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-21 19:08:38 +0300

    update

--- free_stack.c
+++ free_stack.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 /**
-* free_stack - frees a doubly linked list
+* free_stack - frees the stack
 * @head: head of the stack
 */
 void free_stack(stack_t *head)
 {
 	stack_t *aux;
 
-	aux = head;
 	while (head)
 	{
 		aux = head->next;
