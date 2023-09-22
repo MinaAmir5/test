f7b303d618d8b6ee3a4e43b0b14bf6c84d2dcffb
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-09-22 03:15:02 +0300

    Create Void_AddQueue.c

---
+++ Void_AddQueue.c
@@ -0,0 +1,28 @@
+/**
+ * Void_AddQueue - add node to the stack tail
+ * @Copy_U32_Value: new value
+ * @Copy_Stack_Head: stack head
+*/
+void Void_AddQueue(stack_t **Copy_Stack_Head, int Copy_U32_Value)
+{
+	stack_t *Local_Stack_NewNode, *Local_Stack_Tail;
+
+	Local_Stack_NewNode = malloc(sizeof(stack_t));
+	if (Local_Stack_NewNode == NULL)
+		printf("Error\n");
+	Local_Stack_Tail = *Copy_Stack_Head;
+	while ((*Copy_Stack_Head) && Local_Stack_Tail->next)
+		Local_Stack_Tail = Local_Stack_Tail->next;
+	Local_Stack_NewNode->next = NULL;
+	Local_Stack_NewNode->n = Copy_U32_Value;
+	if (!Local_Stack_Tail)
+	{
+		Local_Stack_NewNode->prev = NULL;
+		*Copy_Stack_Head = Local_Stack_NewNode;
+	}
+	else
+	{
+		Local_Stack_NewNode->prev = Local_Stack_Tail;
+		Local_Stack_Tail->next = Local_Stack_NewNode;
+	}
+}
